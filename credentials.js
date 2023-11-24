a9f237e760cb2fb2b433dd898c3f60019c18aa0e
Eddwin Paz <pazeddwin@gmail.com>
2023-11-24 04:29:04 -0300

    change

--- credentials.js
+++ credentials.js
@@ -3,7 +3,7 @@ import puppeteer from 'puppeteer';
 async function takeScreenshot(url) {
     try {
         const url = "https://dev.azure.com/MetLife-Global/Chile%20Digital%20Customer/_git/metlifecl_dc_adn_ts_react_web_svc/pullrequests?_a=active";
-        const browser = await puppeteer.launch({ headless: "false", userDataDir: `/Users/${process.env.MACHINE_NAME}/Library/Application Support/Google/Chrome/Default'`});
+        const browser = await puppeteer.launch({ headless: false, userDataDir: `/Users/${process.env.MACHINE_NAME}/Library/Application Support/Google/Chrome/Default'`});
         const page = await browser.newPage();
         await page.goto(url);
     } catch (error) {
