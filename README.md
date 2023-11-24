8ebf9beeb34037b0db3131bd064e60009e2345eb
Eddwin Paz <pazeddwin@gmail.com>
2023-11-24 04:22:46 -0300

    credential install config

---
+++ README.md
@@ -0,0 +1,9 @@
+## Discord Bot 
+
+1. change .env values
+2. run  
+
+```sh
+npm run dc:register
+npm run dc:start
+```
---
+++ credentials.js
@@ -0,0 +1,12 @@
+async function takeScreenshot(url) {
+    try {
+        const browser = await puppeteer.launch({ headless: "false", userDataDir: `/Users/${process.env.MACHINE_NAME}}/Library/Application Support/Google/Chrome/Default'`});
+        const page = await browser.newPage();
+        await page.goto(url);
+        return filename
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+await takeScreenshot()
